161c31c5b59d721d639581193d9a21a41d3a6a2d
Vatsaev Aslan <a.vatsaev@gmail.com>
2017-08-21 03:14:39 +0200

    feat: added notes crud events

--- actions.js
+++ actions.js
@@ -1,5 +1,13 @@
 module.exports = {
-    NOTE_ADDED: '[Notes] Added',
-    NOTE_UPDATED: '[Notes] Updated',
-    NOTE_DELETED: '[Notes] Deleted'
+
+
+  ADD_NOTE: '[Notes] Add',
+  UPDATE_NOTE: '[Notes] Update',
+  DELETE_NOTE: '[Notes] Delete',
+  LIST_NOTES: '[Notes] List',
+
+  NOTE_ADDED: '[Notes] Added',
+  NOTE_UPDATED: '[Notes] Updated',
+  NOTE_DELETED: '[Notes] Deleted',
+  NOTES_LISTED: '[Notes] Listed'
 };
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const _ = require('lodash');
+const uuid = require('uuid');
 const actions = require('./actions');
 
 
@@ -19,13 +20,11 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-
 const router = express.Router();              // get an instance of the express Router
 
 
 router.get('/', (req, res) => {
-    res.json({ health: 'OK' });
+    res.json({ health: 'OK', ...db });
 });
 
 
@@ -37,44 +36,84 @@ const server = app.listen(app.get('port'), () => {
 
 const io = socketio(server, {'origins': '*:*'} );
 
-
 const db = {
     posts: {},
     notes: {}
 };
 
-createNote = (newNote) => {};
+const createNote = (newNote) => {
+  newNote.id = uuid.v4();
+  if(!newNote.body) newNote.body = '';
+  db.notes[newNote.id] = newNote;
+  return db.notes[newNote.id];
+};
 
 
-io.on('connection', (client) =>{
+const updateNote = (noteData) => {
+  const note = db.notes[noteData.id];
+  if(note) {
+    db.notes[noteData.id] = _.merge(note, noteData);
+    return db.notes[noteData.id];
+  } else return undefined;
+};
+
+const deleteNote = (id) => {
+  if(db.notes[id]){
+    delete db.notes[id];
+  }
+  return id;
+};
+
+function jsUcfirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+io.on('connection', (client) => {
 
     console.log("client connected...");
 
     client.on("join", (data) => {
         console.log(data);
         console.log(`user ${data.username} tries to join ${data.room}`);
-        if(['notes', 'posts'].includes(data.room)){
-            console.log(`client joined ${data.room}`)
+        if(Object.keys(db).includes(data.room)){
+            console.log(`client joined ${data.room}`);
             client.join(data.room);
+            client.emit(`[${jsUcfirst(data.room)}] Listed`, db[data.room])
         }else{
             console.warn('unknown channel')
         }
     });
 
+    client.on(actions.ADD_NOTE, (note) => {
+        const newNote = createNote(note);
+        console.log('add note', newNote);
+        client.to('notes').emit(actions.NOTE_ADDED, newNote);
+        client.emit('[Notes] Listed', db.notes);
+    });
 
+    client.on(actions.LIST_NOTES, () => {
+      client.emit(actions.NOTES_LISTED, db.notes);
+    });
 
-    client.on("addNote", (note) => {
+    client.on(actions.UPDATE_NOTE, (note) => {
+      const updatedNote = updateNote(note);
+      console.log('update note', updatedNote);
+      client.to('notes').emit(actions.NOTE_UPDATED, updatedNote);
+      client.emit('[Notes] Listed', db.notes);
+    });
 
-        console.log('add note', note);
-        db[note.id] = note;
-        client.to('notes').emit(actions.NOTE_ADDED, note);
-
+    client.on(actions.DELETE_NOTE, ({id}) => {
+      console.log("ID", id);
+      deleteNote(id);
+      client.to('notes').emit(actions.NOTE_DELETED, id);
+      client.emit('[Notes] Listed', db.notes);
+      console.log(db.notes)
     });
 
 
     client.on('disconnect', () => {
-        console.log('client disconnected')
-
+      console.log('client disconnected')
     });
 
 });
